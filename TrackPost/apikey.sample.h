9e3a159a1fbe650b8b7f1c5d8279b51ce0a58f02
Osamu Noguchi <atrac613@gmail.com>
2011-04-18 22:53:37 +0900

    add apikey.h template file.

---
+++ TrackPost/apikey.sample.h
@@ -0,0 +1,11 @@
+//
+//  apikey.sample.h
+//  TrackPost
+//
+//  Created by Noguchi Osamu on 11/04/18.
+//  Copyright 2011 envision. All rights reserved.
+//
+
+#define API_KEY "api_key"
+#define API_SECRET "api_secret"
+#define API_URL "https://ws.audioscrobbler.com/2.0/"
